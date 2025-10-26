5079dc8a8fadc31f75b8bab5192caf43232fd7d0
Isabela de Ponte <isabelapnt01@gmail.com>
2025-10-26 17:12:48 -0300

    feat: cria DTOs para Usuario

---
+++ backend/src/main/java/controle/financeiro/backend/dto/request/AtualizaUsuarioDTO.java
@@ -0,0 +1,18 @@
+package controle.financeiro.backend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AtualizaUsuarioDTO {
+    @Email(message = "Email inválido")
+    private String email;
+
+    @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres")
+    private String nomeUsuario;
+}
